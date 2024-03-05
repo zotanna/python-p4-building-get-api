17217285c1eada7d76ea394d32ba4c7895fe1d79
denis <denniiskim@gmail.com>
2024-03-05 03:25:30 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -19,5 +19,40 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
+@app.route('/games')
+def games():
+
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(
+        jsonify(games),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter_by(id=id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(
+        jsonify(game_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = 'games'
+
+    serialize_rules = ('-reviews.game',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -23,9 +21,11 @@ class Game(db.Model):
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
-    
+
+    serialize_rules = ('-game.reviews', '-user.reviews',)
+
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
@@ -38,12 +38,14 @@ class Review(db.Model):
     def __repr__(self):
         return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
+    serialize_rules = ('-reviews.user',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
